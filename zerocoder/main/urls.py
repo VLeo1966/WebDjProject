27c51d2a8b6f15029c5f56afdbe8c654a7440aa0
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-09-21 01:46:20 +0300

    do lesson

--- zerocoder/main/urls.py
+++ zerocoder/main/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),  # Ссылка на приложение main
-    path('new/', views.new),  # Ссылка на приложение weather
-    path('data/', views.data_view, name='data'),
-    path('test/', views.test_view, name='test'),
+    path('', views.index, name='home'),
+    path('new/', views.new, name='page2')
 ]
--- zerocoder/main/views.py
+++ zerocoder/main/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+    data = {
+        'caption': "CatDjango"
+    }
+    return render(request, 'main/index.html',  data)
 
 def new(request):
-    return HttpResponse("<h1>Это вторая страница моего проекта на Django</h1>")
-
-def data_view(request):
-    return HttpResponse('<h1>Data Page</h1><p>Это страница с данными.</p>')
-
-def test_view(request):
-    return HttpResponse('<h1>Test Page</h1><p>Это страница с тестовым содержимым.</p>')
+    return render(request, 'main/new.html')
 
