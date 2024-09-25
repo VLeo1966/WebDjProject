22023c911d2fb6e255aaa1307c7dd4645b1bd6d3
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-09-25 22:17:42 +0300

    yes

--- zerocoder/main/urls.py
+++ zerocoder/main/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('new/', views.new, name='page2')
+    path('new/', views.new, name='page2'),
+    path('news/', include('news.urls'))
 ]
