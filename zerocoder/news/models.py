ff6ab6df6c33e18af5d1b5a779cc0dd530da5c25
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-09-26 20:33:27 +0300

    Сделал домашку

--- zerocoder/news/models.py
+++ zerocoder/news/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 class News_post(models.Model):
-	title = models.CharField('Название новости', max_length=50)
-	short_description = models.CharField('Краткое описание новости', max_length=200)
-	text = models.TextField('Новость')
-	pub_date = models.DateTimeField('Дата публикации')
+	title = models.CharField('Название новости', null=True, max_length=50)
+	short_description = models.CharField('Краткое описание новости', null=True, max_length=200)
+	text = models.TextField('Новость', null=True)
+	autor = models.TextField('Автор статьи', null=True)
+	pub_date = models.DateTimeField('Дата публикации', null=True)
 	def __str__(self):
 		return self.title
 	class Meta:
