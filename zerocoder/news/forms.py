4bee01eadce5866888fe4953e07ef434d37d423f
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-09-27 19:18:45 +0300

    yes

--- zerocoder/news/forms.py
+++ zerocoder/news/forms.py
@@ -1,7 +1,13 @@
-from .models import New_post
-from django.forms import ModelForm
+from .models import News_post
+from django.forms import ModelForm, TextInput, Textarea, DateTimeInput
 
 class News_postForm(ModelForm):
 	class Meta:
-		model = New_post
+		model = News_post
 		fields = ['title', 'short_description', 'text', 'autor', 'pub_date']
+		widgets = {'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Заголовок новости'}),
+				  'short_description': TextInput(attrs={'class': 'form-control', 'placeholder': 'Краткое описание новости'}),
+				  'text': Textarea(attrs={'class': 'form-control', 'placeholder': 'Содержание новости'}),
+				  'autor': TextInput(attrs={'class': 'form-control', 'placeholder': 'Автор статьи'}),
+				  'pub_date': DateTimeInput(attrs={'class': 'form-control', 'placeholder': 'Дата публикации'})
+		}
